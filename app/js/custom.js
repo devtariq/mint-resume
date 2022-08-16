7f2e16217a966148f154745405cd002a8298cc9d
metariqul <metariqul91@gmail.com>
2022-08-16 22:09:25 +0600

    portfolio added

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,6 +1,20 @@
 (function ($) {
     "use strict";
 
-      
+    $('.masson-grid').isotope({
+        itemSelector: '.portfolio-item',
+        percentPosition: true,
+        masonry: {
+          // use outer width of grid-sizer for columnWidth
+          columnWidth: '.grid-sizer',
+          gutter: 30,
+        }
+      });
+
+      $(".portfolio-menu ul li a").on("click", function(e) {
+        e.preventDefault();
+        let filterValue = $(this).attr('data-filter');
+        $('.masson-grid').isotope({ filter: filterValue });
+      })
 
 })(jQuery);
--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,2 @@
-jQuery;
+!function(t){"use strict";t(".masson-grid").isotope({itemSelector:".portfolio-item",percentPosition:!0,masonry:{columnWidth:".grid-sizer",gutter:30}}),t(".portfolio-menu ul li a").on("click",(function(i){i.preventDefault();let o=t(this).attr("data-filter");t(".masson-grid").isotope({filter:o})}))}(jQuery);
 //# sourceMappingURL=custom.js.map
