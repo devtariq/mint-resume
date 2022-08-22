0d1a856cb55f99091de7286c12e2a567a39cb4ed
metariqul <metariqul91@gmail.com>
2022-08-22 07:45:05 +0600

    jquery

--- app/js/custom.js
+++ app/js/custom.js
@@ -17,12 +17,30 @@
         $('.masson-grid').isotope({ filter: filterValue });
       });
 
+    // change active class on buttons
+    $('.portfolio-menu').each(function (i,buttonGroup) {
+        let $buttonGroup = $(buttonGroup);
+        $buttonGroup.on('click', 'a', function () {
+            $buttonGroup.find('.active').removeClass('active');
+            $(this).addClass('active');
+        });
+    });
+
+
       $(".branding-wrap").owlCarousel({
           loop: true,
           autoPlay: true,
           items: 6,
           margin: 30,
           nav: true,
+      });
+
+      $.scrollUp({
+        scrollName: 'scrollUp', // Element ID
+        topDistance: '300', // Distance from top before showing element (px)
+      
+        scrollText: 'Back to Top <i class="fa fa-angle-up"></i>', // Text for element
+        activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
       });
 
 })(jQuery);
